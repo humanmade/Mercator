b07bcb0a198905bb18edfc0bd0b0f861f7dcc1c1
Seth Carstens <seth.carstens@gmail.com>
2015-12-08 14:53:22 -0700

    fix password reset link

--- mercator.php
+++ mercator.php
@@ -291,6 +291,7 @@ function register_mapped_filters() {
 	add_filter( 'site_url', __NAMESPACE__ . '\\mangle_url', -10, 4 );
 	add_filter( 'home_url', __NAMESPACE__ . '\\mangle_url', -10, 4 );
 	add_filter( 'content_url', __NAMESPACE__ . '\\mangle_content_url', -10, 2 );
+	add_filter( 'lostpassword_url', __NAMESPACE__ . '\\mangle_content_url', -10, 2 );
 
 	// If on network site, also filter network urls
 	if ( is_main_site() ) {
